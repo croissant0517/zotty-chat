5f7d70abb6a109bfdb6cb5cb4b23a920fcb66873
croissant <wathyic@gmail.com>
2023-08-27 15:12:26 +0800

    add: add socket.io

--- server/index.js
+++ server/index.js
@@ -4,18 +4,22 @@ const http = require("http");
 const server = http.createServer(app);
 
 const { Server } = require("socket.io");
-// const io = new Server(server, {
-//   cors: {
-//     origin: "*",
-//     methods: ["GET", "POST"],
-//     credentials: true,
-//   },
-// });
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
 
 const port = process.env.PORT || 8000;
 
 app.get("/", (req, res) => {
   res.send("Sever is running!");
+});
+
+io.on("connection", (socket) => {
+  console.log({ socket }, "a user connected");
 });
 
 server.listen(port, () => console.log("Node server listening on port " + port));
